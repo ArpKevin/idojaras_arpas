f95cba635dcf2ae8227e7cb871f82fadb012d4d6
Kevin <arpaskevin11@gmail.com>
2024-04-29 14:02:54 +0200

    Source Code Restructured.

--- idojaras_arpas/MainWindow.xaml.cs
+++ idojaras_arpas/MainWindow.xaml.cs
@@ -1,175 +1,55 @@
-﻿using System;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
+﻿using System.Collections.ObjectModel;
 using System.Windows;
+using System.Windows.Controls;
 
-namespace idojaras_arpas
+namespace ListBoxHoverAttributes
 {
-    // Az időjárás osztály, amely tartalmazza az időjárási adatokat
-    public class WeatherData : INotifyPropertyChanged
-    {
-        private string cityName;
-        public string CityName
-        {
-            get { return cityName; }
-            set
-            {
-                cityName = value;
-                OnPropertyChanged("CityName");
-            }
-        }
-
-        private double temperature;
-        public double Temperature
-        {
-            get { return temperature; }
-            set
-            {
-                temperature = value;
-                OnPropertyChanged("Temperature");
-            }
-        }
-
-        private int humidity;
-        public int Humidity
-        {
-            get { return humidity; }
-            set
-            {
-                humidity = value;
-                OnPropertyChanged("Humidity");
-            }
-        }
-
-        private double windSpeed;
-        public double WindSpeed
-        {
-            get { return windSpeed; }
-            set
-            {
-                windSpeed = value;
-                OnPropertyChanged("WindSpeed");
-            }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-        private void OnPropertyChanged(string propertyName)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-    }
-
-    // Az alkalmazás fő ViewModel osztálya
-    public class MainViewModel : INotifyPropertyChanged
-    {
-        public ObservableCollection<WeatherData> Cities { get; set; }
-
-        private WeatherData selectedCity;
-        public WeatherData SelectedCity
-        {
-            get { return selectedCity; }
-            set
-            {
-                selectedCity = value;
-                OnPropertyChanged("SelectedCity");
-            }
-        }
-
-        private string newCityName;
-        public string NewCityName
-        {
-            get { return newCityName; }
-            set
-            {
-                newCityName = value;
-                OnPropertyChanged("NewCityName");
-            }
-        }
-
-        private double newTemperature;
-        public double NewTemperature
-        {
-            get { return newTemperature; }
-            set
-            {
-                newTemperature = value;
-                OnPropertyChanged("NewTemperature");
-            }
-        }
-
-        private int newHumidity;
-        public int NewHumidity
-        {
-            get { return newHumidity; }
-            set
-            {
-                newHumidity = value;
-                OnPropertyChanged("NewHumidity");
-            }
-        }
-
-        private double newWindSpeed;
-        public double NewWindSpeed
-        {
-            get { return newWindSpeed; }
-            set
-            {
-                newWindSpeed = value;
-                OnPropertyChanged("NewWindSpeed");
-            }
-        }
-
-        public MainViewModel()
-        {
-            Cities = new ObservableCollection<WeatherData>();
-        }
-
-        public void AddCity()
-        {
-            Cities.Add(new WeatherData
-            {
-                CityName = NewCityName,
-                Temperature = NewTemperature,
-                Humidity = NewHumidity,
-                WindSpeed = NewWindSpeed
-            });
-        }
-
-        public void RemoveCity()
-        {
-            if (SelectedCity != null)
-            {
-                Cities.Remove(SelectedCity);
-                SelectedCity = null;
-            }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-        private void OnPropertyChanged(string propertyName)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-    }
-
     public partial class MainWindow : Window
     {
-        public MainViewModel ViewModel { get; set; }
+        public ObservableCollection<Item> Items { get; set; }
 
         public MainWindow()
         {
             InitializeComponent();
-            ViewModel = new MainViewModel();
-            DataContext = ViewModel;
+            DataContext = this;
+
+            // Sample data
+            Items = new ObservableCollection<Item>
+            {
+                new Item { Name = "Item 1", Attribute1 = "Attribute 1 Value", Attribute2 = "Attribute 2 Value", Attribute3 = "Attribute 3 Value" },
+                new Item { Name = "Item 2", Attribute1 = "Attribute 4 Value", Attribute2 = "Attribute 2 Value", Attribute3 = "Attribute 3 Value" },
+                new Item { Name = "Item 3", Attribute1 = "Attribute 5 Value", Attribute2 = "Attribute 2 Value", Attribute3 = "Attribute 3 Value" }
+            };
+
+            listBox.ItemsSource = Items;
+            listBox.SelectionChanged += ListBox_SelectionChanged;
         }
 
-        private void AddCityButton_Click(object sender, RoutedEventArgs e)
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            ViewModel.AddCity();
+            if (listBox.SelectedItem != null)
+            {
+                UpdateAttributeText();
+            }
         }
 
-        private void RemoveCityButton_Click(object sender, RoutedEventArgs e)
+        private void UpdateAttributeText()
         {
-            ViewModel.RemoveCity();
+            if (listBox.SelectedItem != null)
+            {
+                Item selectedItem = (Item)listBox.SelectedItem;
+                attributeTextBlock.Text = $"Attribute 1: {selectedItem.Attribute1}\n" +
+                                          $"Attribute 2: {selectedItem.Attribute2}\n" +
+                                          $"Attribute 3: {selectedItem.Attribute3}";
+            }
         }
+    }
+
+    public class Item
+    {
+        public string Name { get; set; }
+        public string Attribute1 { get; set; }
+        public string Attribute2 { get; set; }
+        public string Attribute3 { get; set; }
     }
 }
